32684dcd84ea6ac1b3a21896b5cb22d20e4f2d2e
pmadinga <phiwen2@gmail.com>
2022-03-10 20:17:04 +0200

    Add firebase config

---
+++ src/auth/signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div>
+            <form action="">
+            <input type="Name" />
+            <input type="Surname" />
+            <input type="email" />
+            </form>
+        </div>
+    );
+}
+
+export default Signup;
